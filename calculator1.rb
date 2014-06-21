9583cbc1c057af3f9cce49bc47099f67c4528030
NationXZephys <akbar.s@bronxsoftware.org>
2014-06-21 14:44:00 -0400

    created ruby calculator

---
+++ calculator1.rb
@@ -0,0 +1,24 @@
+puts "welcome to the calculator"
+puts "please enter a number"
+
+number = gets.chomp!.to_i 
+
+puts "Enter an operator(+, -, *, /)"
+
+operator = gets.chomp!
+
+puts "please enter another number"
+
+second_number = gets.chomp!.to_i
+
+if operator == "+"
+  puts number + second_number 
+elsif operator == "-"
+  puts number - second_number 
+elsif operator == "*"
+  puts number * second_number 
+elsif operator == "/"
+  puts number/second_number
+else 
+  puts "incorrect operator"
+end 
